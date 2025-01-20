9dcf64da1ffe036c675fcca82f8c587e36839cb9
J Park <mrparkonline@gmail.com>
2025-01-20 12:17:37 -0500

    added tabs per unit

--- ics4u.py
+++ ics4u.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
-st.write("ICS4U App.")
+# Title
+st.title("ICS4U Landing Page")
+st.write("Grade 12 Computer Science Content.")
+# END OF TITLE
+
+# Content
+# Navigation:
+tab1, tab2, tab3, tab4 = st.tabs(["U1: Python Programming", "U2: Algorithmic Thinking", "U3: Object Oriented Programming", "U4: Databases"])
+
+with tab1:
+    st.write("Python Programming")
+
+with tab2:
+    st.write("Algorithmic Thinking")
+
+with tab3:
+    st.write("Object Oriented Programming")
+
+with tab4:
+    st.write("Databases")
+
+# END OF CONTENT
